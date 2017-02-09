ba2f8c1f3eb170cd711398babb6f7e2435afad31
Marlon Martinez <jmarlonmartinez13@gmail.com>
2017-02-09 12:45:20 -0500

    move right move left

--- js/game-2048.js
+++ js/game-2048.js
@@ -57,3 +57,87 @@ Game2048.prototype._getAviablePosition = function () {
       console.log(row);
     });
   };
+
+  Game2048.prototype.moveLeft = function () {
+    var updatedBoard = [];
+
+    this.board.forEach(function (row) {
+      // 1 remove empty from row
+      var newRow = []; // in here are the number that are in the row
+
+      row.forEach( function (cell) {
+        if (cell !== null){
+          newRow.push(cell);
+        }
+      });
+
+      // 2 merge tile in the row that are together and the same number
+      for (var i = 0; i < newRow.length; i += 1) {
+        // e.g [8, 8, 4] -> [16, null, 4]
+        if (newRow[i] === newRow[i + 1]) {
+          newRow[i] *= 2;
+          newRow[i + 1] = null;
+        }
+      }
+      //3 remove new empties in the middle
+      // e.g [8, 8, 4] -> [16, 4] getting the null out
+      var moved = [];
+
+      newRow.forEach( function (cell) {
+        if (cell !== null){
+          moved.push(cell);
+        }
+      });
+
+      // 4 push() nulls until row has length 4 again
+      while (moved.length < 4) {
+        moved.push(null);
+      }
+
+      updatedBoard.push(moved);
+    });
+
+      this.board = updatedBoard;
+  };
+
+  Game2048.prototype.moveRight = function () {
+    var updatedBoard = [];
+
+    this.board.forEach(function (row) {
+      // 1 remove empty from row
+      var newRow = []; // in here are the number that are in the row
+
+      row.forEach( function (cell) {
+        if (cell !== null){
+          newRow.push(cell);
+        }
+      });
+
+      // 2 merge tile in the row that are together and the same number
+      for (var i = newRow.length - 1; i >= 0; i -= 1) {
+        // e.g [8, 8, 4] -> [16, null, 4]
+        if (newRow[i] === newRow[i + 1]) {
+          newRow[i] *= 2;
+          newRow[i - 1] = null;
+        }
+      }
+      //3 remove new empties in the middle
+      // e.g [8, 8, 4] -> [16, 4] getting the null out
+      var moved = [];
+
+      newRow.forEach( function (cell) {
+        if (cell !== null){
+          moved.push(cell);
+        }
+      });
+
+      // 4 push() nulls until row has length 4 again
+      while (moved.length < 4) {
+        moved.unshift(null);
+      }
+
+      updatedBoard.unshift(moved);
+    });
+
+      this.board = updatedBoard;
+  };
